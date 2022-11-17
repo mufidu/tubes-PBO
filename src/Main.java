c6d1d1df05dcac0d2ad4bfb15276ef8b92b2b7b2
mufidu <mufidu@outlook.com>
2022-11-17 22:42:01 +0700

    Implement show friends and log out

--- src/Main.java
+++ src/Main.java
@@ -53,70 +53,89 @@ public class Main {
         }
         System.out.println("");
         
-        OUTER:
         while (true) {
-            System.out.println("Apa yang ingin Anda lakukan?");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Next menu");
-            System.out.print("Pilihan (1-3): ");
-            int pil = s.nextInt();
-            
-            switch (pil) {
-                case 1:
-                    numOfUser++;
-                    tempUserList = userList;
-                    userList = new UserModel[numOfUser];
-                    System.arraycopy(tempUserList, 0, userList, 0, tempUserList.length);
-                    auth.registerUser(userList, numOfUser);
-                    break;
-                case 2:
-                    double userId = auth.loginUser(userList);
-                    if (userId != -1){
-                        System.out.println("");
-                        System.out.println("=======Selamat Datang!======");
-                        UserModel u = user.getUserById(userList, userId);
-                        System.out.println("Username: " + u.getUsername());
-                        System.out.println("Friends: " + Arrays.toString(u.getFriends()));
-                        System.out.println("");
-                        currentId = userId;
-                        currentUser = user.getUserById(userList, currentId);
-                    }
-                    else {
-                        System.out.println("\nUsername atau password yang dimasukkan salah!\n");
-                    }   break;
-                case 3:
-                    if (currentId == 0) {
-                        System.out.println("\nMaaf Anda harus login terlebih dahulu!\n");
-                    } else {
-                        System.out.println("\n============================\n");
-                        break OUTER;
-                    }
-                    break;
-                default:
-                    break;
+            OUTER:
+            while (true) {
+                System.out.println("Apa yang ingin Anda lakukan?");
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println("3. Next menu");
+                System.out.print("Pilihan (1-3): ");
+                int pil = s.nextInt();
+
+                switch (pil) {
+                    case 1:
+                        numOfUser++;
+                        tempUserList = userList;
+                        userList = new UserModel[numOfUser];
+                        System.arraycopy(tempUserList, 0, userList, 0, tempUserList.length);
+                        auth.registerUser(userList, numOfUser);
+                        break;
+                    case 2:
+                        double userId = auth.loginUser(userList);
+                        if (userId != -1){
+                            System.out.println("");
+                            System.out.println("=======Selamat Datang!======");
+                            UserModel u = user.getUserById(userList, userId);
+                            System.out.println(u.getUsername()
+                            + " berhasil login!");
+                            System.out.println("");
+                            currentId = userId;
+                            currentUser = user.getUserById(userList, currentId);
+                        }
+                        else {
+                            System.out.println("\nUsername atau password yang dimasukkan salah!\n");
+                        }   break;
+                    case 3:
+                        if (currentId == 0) {
+                            System.out.println("\nMaaf Anda harus login terlebih dahulu!\n");
+                        } else {
+                            System.out.println("\n============================\n");
+                            break OUTER;
+                        }
+                        break;
+                    default:
+                        System.out.println("\nPilihan invalid!\n");
+                }
             }
-        }
-        
-        while (true) {            
-            System.out.println("Apa yang ingin Anda lakukan?");
-            System.out.println("1. Tambah teman");
-            System.out.println("2. Info profil");
-            
-            System.out.print("Pilihan (1): ");
-            int pil = s.nextInt();
-            
-            if (pil == 1) {
-                System.out.print("Apa username teman Anda? ");
-                String friendUsername = s.next();
-                UserModel friend = user.getUserByUsername(userList, friendUsername);
-                user.addFriends(currentUser, friend);
-            } else if (pil == 2) {
-                UserModel u = user.getUserById(userList, currentId);
-                System.out.println("\nHalo, "+u.getUsername()+"!");
-                System.out.println("Username: " + u.getUsername());
-                System.out.println("Friends: " + Arrays.toString(u.getFriends()));
-                System.out.println("");
+
+            OUTER_1:            
+            while (true) {
+                System.out.println("Apa yang ingin Anda lakukan?");
+                System.out.println("1. Tambah teman");
+                System.out.println("2. Info profil");
+                System.out.println("3. Log out");
+                System.out.print("Pilihan (1): ");
+                int pil = s.nextInt();
+
+                switch (pil) {
+                    case 1:
+                        System.out.print("Apa username teman Anda? ");
+                        String friendUsername = s.next();
+                        UserModel friend = user.getUserByUsername(userList, friendUsername);
+                        user.addFriends(currentUser, friend);
+                        System.out.println("\n"+friend.getUsername()
+                                +" berhasi ditambahkan sebagai teman.\n");
+                        break;
+                    case 2:
+                        UserModel u = user.getUserById(userList, currentId);
+                        System.out.println("\nHalo, "+u.getUsername()+"!");
+                        System.out.println("Username: " + u.getUsername());
+                        System.out.println("Username teman-teman: ");
+                        double[] friends = u.getFriends();
+                        for (int i = 0; i < friends.length; i++) {
+                            int no = i+1;
+                            System.out.println(no+". "
+                                    + user.getUserById(userList, friends[i]).getUsername());
+                        }   System.out.println("");
+                        break;
+                    case 3:
+                        currentId = 0;
+                        System.out.println("\nAnda telah log out.\n");
+                        break OUTER_1;
+                    default:
+                        System.out.println("\nPilihan invalid!\n");
+                }
             }
         }
     }
